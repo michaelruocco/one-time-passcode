cbdcabefcc90c25f12180c9c040bc5c4292f7e07
Michael Ruocco <michael.ruocco@hotmail.com>
2021-03-27 15:45:40 +0000

    removes publishing to latest docker image and uses script for triggering platform pipeline

---
+++ scripts/trigger-platform-pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+username=$1
+password=$2
+
+version=$(./gradlew currentVersion -q -Prelease.quiet)
+body='{"ref": "master", "inputs": { "serviceName": "idv-one-time-passcode", "serviceVersion": "VERSION" } }'
+body=$(echo "$body" | sed "s|VERSION|$version|")
+
+curl -XPOST -u "${username}:${password}" \
+  -H "Accept: application/vnd.github.everest-preview+json" \
+  -H "Content-Type: application/json" \
+  "https://api.github.com/repos/michaelruocco/idv-platform/actions/workflows/update-service-pipeline.yml/dispatches" \
+   --data "$body"
