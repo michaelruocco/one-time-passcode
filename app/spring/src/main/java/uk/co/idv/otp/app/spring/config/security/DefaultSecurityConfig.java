4f115dbfae9f8d7b850abaf0e1d4c495b57c773e
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-04 01:58:06 +0100

    updates postman collection to pass auth tokens

--- app/spring/src/main/java/uk/co/idv/otp/app/spring/config/security/DefaultSecurityConfig.java
+++ app/spring/src/main/java/uk/co/idv/otp/app/spring/config/security/DefaultSecurityConfig.java
@@ -1,16 +1,14 @@
 package uk.co.idv.otp.app.spring.config.security;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -37,37 +35,38 @@ public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .mvcMatchers("/actuator/**").permitAll()
                 .mvcMatchers("/v1/**").authenticated()
-                .and().cors()
                 .and().oauth2ResourceServer().jwt();
     }
 
     @Bean
-    @ConditionalOnProperty(value = "spring.security.oauth2.resourceserver.jwt.jwks-override-uri")
-    public JwtDecoder overrideJwtDecoder() {
-        log.warn("jwt decoder overridden using jwks override url {}, this should only be enabled for local demo or testing purposes", jwksOverrideUri);
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwksOverrideUri).build();
-        jwtDecoder.setJwtValidator(tokenValidator());
-        return jwtDecoder;
-    }
-
-    @Bean
-    @Profile("insecure")
+    @Profile("stubbed")
     public JwtDecoder stubbedJwtDecoder(Clock clock) {
         log.warn("allow all jwt decoder enabled, this should only be enabled for local demo or testing purposes");
         return new AllowAllJwtDecoder(clock);
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = JwtDecoder.class)
-    public JwtDecoder defaultJwtDecoder() {
+    @Profile("!stubbed")
+    public JwtDecoder jwtDecoder() {
+        if (StringUtils.isEmpty(jwksOverrideUri)) {
+            return defaultJwtDecoder();
+        }
+        return overrideJwtDecoder();
+    }
+
+    private JwtDecoder defaultJwtDecoder() {
         log.info("default jwt decoder enabled using issuer {}", issuer);
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(tokenValidator());
+        return jwtDecoder;
+    }
+
+    private JwtDecoder overrideJwtDecoder() {
+        log.warn("jwt decoder overridden using jwks override url {}, this should only be enabled for local demo or testing purposes", jwksOverrideUri);
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwksOverrideUri).build();
         jwtDecoder.setJwtValidator(tokenValidator());
         return jwtDecoder;
     }
